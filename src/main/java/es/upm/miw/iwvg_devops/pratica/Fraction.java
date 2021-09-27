b17059ae11d47e29c00c13f8e29da5d811b60e79
netoap <alexanderneto5@gmail.com>
2021-09-27 21:13:46 +0200

    Added UsersDatabase #8

--- src/main/java/es/upm/miw/iwvg_devops/pratica/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/pratica/Fraction.java
@@ -1,4 +1,16 @@
 package es.upm.miw.iwvg_devops.pratica;
 
 public class Fraction {
+    private int numerator;
+
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
 }
