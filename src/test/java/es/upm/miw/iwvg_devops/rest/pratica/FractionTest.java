d7dbb716bc768ed6ad17cecebb053c3a3d04c139
netoap <alexanderneto5@gmail.com>
2021-09-29 23:24:32 +0200

    Enhanced FractionTest and UserTest #9

--- src/test/java/es/upm/miw/iwvg_devops/rest/pratica/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/pratica/FractionTest.java
@@ -1,4 +1,74 @@
 package es.upm.miw.iwvg_devops.rest.pratica;
 
+import es.upm.miw.iwvg_devops.pratica.Fraction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FractionTest {
+    private Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        fraction = new Fraction(2, 3);
+    }
+
+    @Test
+    void testFractionIntInt() {
+        assertEquals(2, fraction.getNumerator());
+        assertEquals(3, fraction.getDenominator());
+        assertEquals(fraction, new Fraction(20, 30));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Fraction(1, 0));
+        assertTrue(exception.getMessage().contains("Argument 'denominator' is 0"));
+    }
+
+    @Test
+    void testFraction() {
+        fraction = new Fraction();
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+
+    @Test
+    void testDecimal() {
+        fraction = new Fraction(3, 78);
+        assertEquals(0.0384, fraction.decimal(), 0.001);
+    }
+
+    @Test
+    void testIsProper() {
+        fraction = new Fraction(3, 8);
+        assertTrue(fraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertTrue(new Fraction(8, 3).isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        Fraction fraction1 = new Fraction();
+        fraction1.setNumerator(6);
+        fraction1.setDenominator(9);
+        assertTrue(fraction.isEquivalent(fraction1));
+    }
+
+    @Test
+    void testAdd() {
+        assertEquals(fraction.add(new Fraction(3, 3)), new Fraction(5, 3));
+        assertEquals(fraction.add(new Fraction(1, 2)), new Fraction(7, 6));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(fraction.multiply(new Fraction(5, 15)), new Fraction(2, 9));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(fraction.divide(new Fraction(1, 5)), new Fraction(10, 3));
+    }
 }
--- src/test/java/es/upm/miw/iwvg_devops/rest/pratica/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/pratica/UserTest.java
@@ -1,8 +1,61 @@
 package es.upm.miw.iwvg_devops.rest.pratica;
 
+import es.upm.miw.iwvg_devops.pratica.Fraction;
 import es.upm.miw.iwvg_devops.pratica.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.platform.commons.util.CollectionUtils.*;
 
 public class UserTest {
 
+    private User user;
+
+    @BeforeEach
+    void before() {
+        List<Fraction> fractions = new ArrayList<>();
+        fractions.add(new Fraction(1, 2));
+        fractions.add(new Fraction(2, 3));
+        user = new User("1", "John", "Doe", fractions);
+    }
+
+    @Test
+    void testUser() {
+        assertEquals("1", user.getId());
+        assertEquals("John", user.getName());
+        assertEquals("Doe", user.getFamilyName());
+        assertEquals(new Fraction(1, 2), user.getFractions().get(0));
+        assertEquals(List.of(
+                new Fraction(1, 2),
+                new Fraction(2, 3)
+        ), user.getFractions());
+    }
+
+    @Test
+    void testAddFraction() {
+        List<Fraction> expectedFractions = List.of(
+                new Fraction(1, 2),
+                new Fraction(2, 3),
+                new Fraction(3, 4));
+        user.addFraction(new Fraction(3, 4));
+        assertEquals(expectedFractions, user.getFractions());
+        assertEquals(3, user.getFractions().size());
+    }
+
+    @Test
+    void testFullName() {
+        assertEquals("John Doe", user.fullName());
+    }
+
+    @Test
+    void testInitials() {
+        assertEquals("J.", user.initials());
+    }
 }
